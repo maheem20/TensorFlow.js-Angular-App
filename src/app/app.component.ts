f3ed0a3a4c79db1d08348ebef136dd97416be2a8
maheem_20 <mwistanbuly@gmail.com>
2023-06-11 01:59:54 +0200

    define trainNewModel() function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,21 @@ import * as tf from '@tensorflow/tfjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tensorApp';
 
   linearModel: tf.Sequential;
   prediction: any;
+
+  ngOnInit() {
+    this.trainNewModel();
+  }
+
+  async trainNewModel() {
+    // Define a model for linear regression.
+    this.linearModel = tf.sequential();
+    this.linearModel.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+    // Prepare the model for training: Specify the loss and the optimizer.
+    this.linearModel.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+  }
 }
